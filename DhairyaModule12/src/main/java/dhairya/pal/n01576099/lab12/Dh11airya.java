ffd99ab4c47ed93b4373afb035339e06e42e3d64
DhairyaPal6099 <n01576099@humber.ca>
2025-04-04 21:35:05 -0400

    Created Dh11airya fragment UI.

---
+++ DhairyaModule12/src/main/java/dhairya/pal/n01576099/lab12/Dh11airya.java
@@ -0,0 +1,28 @@
+package dhairya.pal.n01576099.lab12;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Dh11airya extends Fragment {
+
+    public Dh11airya() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dh11airya, container, false);
+    }
+}
