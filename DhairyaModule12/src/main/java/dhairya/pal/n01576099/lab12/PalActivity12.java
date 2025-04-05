d8bed95fb4f8ce92c3a57b4e10fff90d85b38304
DhairyaPal6099 <n01576099@humber.ca>
2025-04-04 20:28:58 -0400

    Created Toolbar and created the toolbar icon to start intent to open contacts

--- DhairyaModule12/src/main/java/dhairya/pal/n01576099/lab12/PalActivity12.java
+++ DhairyaModule12/src/main/java/dhairya/pal/n01576099/lab12/PalActivity12.java
@@ -1,8 +1,14 @@
 package dhairya.pal.n01576099.lab12;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -13,10 +19,27 @@ public class PalActivity12 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.dhaMain), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Toolbar toolbar = findViewById(R.id.dhaToolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+        startActivity(intent);
+        return super.onOptionsItemSelected(item);
     }
 }
