48877856cc9d584d5d3bae965b1b9ed5b46bf97f
DhairyaPal6099 <n01576099@humber.ca>
2025-04-06 23:56:54 -0400

    Finished the notification part of the second fragment as well.

--- DhairyaModule12/src/main/java/dhairya/pal/n01576099/lab12/Pa22l.java
+++ DhairyaModule12/src/main/java/dhairya/pal/n01576099/lab12/Pa22l.java
@@ -2,17 +2,28 @@
 
 package dhairya.pal.n01576099.lab12;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationChannelCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.PermissionChecker;
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
 import android.location.Address;
 import android.location.Geocoder;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -23,13 +34,16 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.io.IOException;
+import java.security.Permission;
 import java.util.List;
 
 public class Pa22l extends Fragment {
     private MapView mapView;
     private GoogleMap gMap;
-
     private TextView textView;
+    ActivityResultLauncher<String> permissionLauncher;
+    StringBuilder fullAddress;
+    NotificationManagerCompat managerCompat;
 
     private static final LatLng SYDNEY = new LatLng(-34, 151);
 
@@ -65,7 +79,7 @@ public class Pa22l extends Fragment {
                             List<Address> addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
                             if (addresses != null && !addresses.isEmpty()) {
                                 Address address = addresses.get(0);
-                                StringBuilder fullAddress = new StringBuilder();
+                                fullAddress = new StringBuilder();
                                 for (int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
                                     fullAddress.append(address.getAddressLine(i)).append(getString(R.string.newline));
                                 }
@@ -81,6 +95,9 @@ public class Pa22l extends Fragment {
                                 Snackbar snackbar = Snackbar.make(view, fullAddress, Snackbar.LENGTH_INDEFINITE);
                                 snackbar.setAction(getString(R.string.dismiss), view1 -> snackbar.dismiss());
                                 snackbar.show();
+
+                                //Notification
+                                showNotification();
                             }
                         } catch (IOException e) {
                             System.out.println(e.getMessage());
@@ -90,6 +107,39 @@ public class Pa22l extends Fragment {
             });
         }
 
+        //Creating notification channel
+        NotificationChannelCompat notificationChannelCompat = new NotificationChannelCompat.Builder("Location Notification Channel", NotificationManagerCompat.IMPORTANCE_HIGH)
+                .setName("Location notification")
+                .setDescription("Notifications for the selected location on Google Map")
+                .setVibrationEnabled(true)
+                .build();
+        managerCompat = NotificationManagerCompat.from(getContext());
+        managerCompat.createNotificationChannel(notificationChannelCompat);
+
+        //Setting up notifications permissions launcher
+        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean o) {
+                if (o) {
+                    showNotification();
+                } else {
+                    Toast.makeText(getContext(), getString(R.string.permission_denied), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    private void showNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.POST_NOTIFICATIONS) != PermissionChecker.PERMISSION_GRANTED) {
+            permissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS);
+        } else {
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(getContext(), "Location Notification Channel")
+                    .setSmallIcon(R.drawable.location_pin)
+                    .setContentTitle("Address change")
+                    .setContentText(fullAddress);
+            managerCompat.notify(1, builder.build());
+        }
     }
 
     @Override
